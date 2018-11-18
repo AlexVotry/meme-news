fb05637a821efd3ce2b25e1dcf1acfc05be792ad
Alex Votry <alexvotry@gmail.com>
2018-11-17 16:08:26 -0800

    photo in story

--- src/app/memes/memes.service.ts
+++ src/app/memes/memes.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 export class MemesService {
   public articles = [
     { headline: "first story",
-      story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      image: "../../assets/bill-hillary.png",
       meme: "../../assets/story1.png",
       hates: 0,
       likes: 0
